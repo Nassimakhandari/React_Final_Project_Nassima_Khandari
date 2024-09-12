2f9c25079111a9aab545c93f224185470e62cec1
Nassimakhandari <nassomanassima37@gmail.com>
2024-09-12 11:36:38 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/home/Home.jsx";
+import About from "./pages/About/About.jsx";
+import "./index.css";
+import Contact from "./pages/About/Contact.jsx";
+import Shop from "./pages/Shop/shop.jsx";
+import Blog from "./pages/Blog/blog.jsx";
+import Navbar from "./layouts/navbar.jsx";
+import Footer from "./layouts/footer.jsx";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
+
---
+++ src/pages/Shop/shop.jsx
@@ -0,0 +1,11 @@
+
+function Shop() {
+  return (
+    <div>
+      <h1>Menu Page</h1>
+    </div>
+  );
+}
+
+export default Shop;
+
