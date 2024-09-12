daa838a5b3ba7066f53f141454a77efa3eb69822
Nassimakhandari <nassomanassima37@gmail.com>
2024-09-13 00:00:14 +0100

    add respo of about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -10,11 +10,11 @@ function About() {
         <h1 className='text-5xl text-white font-bold'>ABOUT</h1>
       </div>
 
-      <div className='flex justify-center items-center gap-6'>
-        <div>
-          <img src={images.img14} alt="" className='w-[21vw] h-[80vh] object-contain' />
+      <div className='flex justify-center items-center gap-6 respo'>
+        <div className='overflow-hidden'>
+          <img src={images.img14} alt="" className='w-[21vw] h-[80vh] object-contain transition-all duration-500 ease-in-out hover:scale-110 about-img' />
         </div>
-        <div className='w-[44%] flex flex-col gap-4'>
+        <div className='lg:w-[44%] md:w-[50%] flex flex-col gap-4 respo-about' >
           <h1 className='text-2xl font-light'>Our story</h1>
           <p className='text-gray-500 font-light'>Phasellus egestas nisi nisi, lobortis ultricies risus semper nec. Vestibulum pharetra ac ante ut pellentesque. Curabitur fringilla dolor quis lorem accumsan, vitae molestie urna dapibus. Pellentesque porta est ac neque bibendum viverra. Vivamus lobortis magna ut interdum laoreet. Donec gravida lorem elit, quis condimentum ex semper sit amet. Fusce eget ligula magna. Aliquam aliquam imperdiet sodales. Ut fringilla turpis in vehicula vehicula. Pellentesque congue ac orci ut gravida. Aliquam erat volutpat. Donec iaculis lectus a arcu facilisis, eu sodales lectus sagittis. Etiam pellentesque, magna vel dictum rutrum, neque justo eleifend elit, vel tincidunt erat arcu ut sem. Sed rutrum, turpis ut commodo efficitur, quam velit convallis ipsum, et maximus enim ligula ac ligula. Vivamus tristique vulputate ultricies. Sed vitae ultrices orci.</p>
           <div className='border-l-4 pl-6 flex flex-col gap-3'>
